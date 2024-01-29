63e76a753f990f286464e8b2139d90381f12d292
DD1C7470R <gracevalley101@gmail.com>
2024-01-29 17:57:23 +0000

    Dock task 0

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -5,5 +5,7 @@ Main file
 
 
 def index_range(page: int = None, page_size: int = None) -> tuple:
+    """Retrieves the index range from a given page and page size.
+    """
     start = (page - 1) * page_size
     return (start, page * page_size)
