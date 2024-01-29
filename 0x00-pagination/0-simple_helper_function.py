b1c9fa21358ef23d1a110297c6da38e2b2d70482
DD1C7470R <gracevalley101@gmail.com>
2024-01-29 17:49:42 +0000

    Change null case

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -5,11 +5,6 @@ Main file
 
 
 def index_range(page: int = None, page_size: int = None) -> tuple:
-    if page and page_size:
-        if page > 1:
-            start = (page * page_size) - page_size
-            return (start, page * page_size)
-        else:
-            return (0, page_size)
-    else:
-        return (None, None)
+    start = (page * page_size) - page_size
+    end = page * page_size
+    return (start, page * page_size)
