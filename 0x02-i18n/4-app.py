c6299eca10bd4639f21aa7b461a2cb02f7c4c326
DD1C7470R <gracevalley101@gmail.com>
2024-02-06 19:37:48 +0000

    Add task 4

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -20,16 +20,16 @@ babel = Babel(app)
 
 @babel.localeselector
 def get_locale() -> str:
-    """Retrieves the locale for a web page.
-    """
-    queries = request.query_string.decode('utf-8').split('&')
-    query_table = dict(map(
-        lambda x: (x if '=' in x else '{}='.format(x)).split('='),
-        queries,
-    ))
-    if 'locale' in query_table:
-        if query_table['locale'] in app.config["LANGUAGES"]:
-            return query_table['locale']
+    """Retrieves the locale for a web page."""
+    try:
+        queries = request.args.to_dict()
+        locale = queries.get('locale')
+        if locale and locale in app.config["LANGUAGES"]:
+            return locale
+    except Exception as e:
+        app.logger.error(f"Error processing request query parameters: {e}")
+
+    # Resort to default behavior if locale not provided or invalid
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
