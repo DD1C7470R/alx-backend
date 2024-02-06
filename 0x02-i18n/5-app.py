901ea42bfee6591c4089796a736cc9d10cfefe94
DD1C7470R <gracevalley101@gmail.com>
2024-02-06 20:36:17 +0000

    Update task 5

---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+""" Starts a Flash Web Application """
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+from typing import Union, Dict
+
+
+class Config:
+    """Represents a Flask Babel configuration.
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.url_map.strict_slahes = False
+app.config.from_object(Config)
+babel = Babel(app)
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user() -> Union[Dict, None]:
+    """Retrieves a user based on a user id.
+    """
+    login_id = request.args.get('login_as')
+    if login_id:
+        return users.get(int(login_id))
+    return None
+
+
+@app.before_request
+def before_request() -> None:
+    """Performs some routines before each request's resolution.
+    """
+    user = get_user()
+    g.user = user
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """Retrieves the locale for a web page."""
+    try:
+        queries = request.args.to_dict()
+        locale = queries.get('locale')
+        if locale and locale in app.config["LANGUAGES"]:
+            return locale
+    except Exception as e:
+        app.logger.error(f"Error processing request query parameters: {e}")
+
+    # Resort to default behavior if locale not provided or invalid
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route('/')
+def get_indexi() -> str:
+    """ Prints a Message when / is called """
+    g.locale = str(get_locale())
+    return render_template('5-index.html')
+
+
+if __name__ == "__main__":
+    """ Main Function """
+    app.run(host='0.0.0.0', port=2000)
